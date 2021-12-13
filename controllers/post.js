e06613891b1f2b829d655298986a0a92ef84ae69
lekkss <leksguy97@gmail.com>
2021-12-13 13:34:02 +0100

    get

--- controllers/post.js
+++ controllers/post.js
@@ -2,9 +2,10 @@ const Post = require("../models/post")
 
 exports.getPosts = (req, res)=> {
     const posts = Post.find()
+    .select("_id title body")
     .then((posts) =>{
         res.status(200).json({
-            posts: posts
+            posts
         })
     })
     .catch(err => console.log(err));
